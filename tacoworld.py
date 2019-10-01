7cd80b56f5b0955daaa91768b41a8f83e8090691
dcfein08 <42851773+dcfein08@users.noreply.github.com>
2019-10-01 19:27:43 -0400

    tacoworld: plenty o tacos

--- tacoworld.py
+++ tacoworld.py
@@ -1,2 +1,2 @@
 print("I, Zach Lamberty, want tacos more than *anyone*, especially more than dan!")
-print("I, Dan Fein, want tacos!!!")
+print("I, Dan Fein, want tacos, so it's good there are enough to share!")
